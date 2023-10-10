5d8ab3c2700122c97c554c511fcc4a4f73f1121c
KiranVdP <100024439+KiranVdP@users.noreply.github.com>
2023-10-10 12:13:17 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+#mkdir tempdir
+#mkdir tempdir/templates
+#mkdir tempdir/static
+
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
